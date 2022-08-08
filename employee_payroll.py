cc972520fb37c95c2287ab20e82f55bd46e04dac
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-08 21:17:47 +0530

    attendence logic added

--- employee_payroll.py
+++ employee_payroll.py
@@ -1,2 +1,13 @@
 # Employee payroll program
+import random
+
 print("Welcome to employee payroll program")
+random_number = random.randint(0, 1)
+
+if random_number == 0:
+    print("Employee is absent")
+elif random_number == 1:
+    print("Employee is present")
+else:
+    print("Invalid")
+
