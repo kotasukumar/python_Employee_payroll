578c361052ea2e2fb409d9575f848b040a795a82
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-08 21:27:08 +0530

    wage calculation logic added

--- employee_payroll.py
+++ employee_payroll.py
@@ -1,13 +1,19 @@
 # Employee payroll program
 import random
 
-print("Welcome to employee payroll program")
-random_number = random.randint(0, 1)
 
-if random_number == 0:
-    print("Employee is absent")
-elif random_number == 1:
-    print("Employee is present")
-else:
-    print("Invalid")
+def employee(random_number):
+    WAGE_PER_HOUR = 20
+    WORKING_HOUR = 8
+    if random_number == 0:
+        print("Employee is absent and his wage is: ", WORKING_HOUR * WAGE_PER_HOUR)
+    elif random_number == 1:
+        print("Employee is present and his wage is zero")
+    else:
+        print("Invalid")
 
+
+if __name__ == "__main__":
+    print("Welcome to employee payroll program")
+    number = random.randint(0, 1)
+    employee(number)
