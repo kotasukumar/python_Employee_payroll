ba8250ee728c091e1b422317d3a430e2c178c701
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-08-08 22:07:07 +0530

    while loop

--- employee_payroll.py
+++ employee_payroll.py
@@ -3,16 +3,23 @@ import random
 
 
 def employee():
+    MIN_WORKING_DAYS = 20
+    MIN_WORKING_HOUR = 100
     WAGE_PER_HOUR = 20
     FULL_WORKING_HOUR = 8
-    PART_TIME_WORKING_HOURS = 4
-    total_wage = 0
-    for i in range(20):
+    PART_TIME_WORKING_HOURS = 8
+    total_wage = total_hours = total_days = 0
+
+    while total_hours <= 100 and total_days <= 20:
         random_number = random.randint(0, 2)
         if random_number == 0:
+            total_hours += FULL_WORKING_HOUR
+            total_days += 1
             total_wage += FULL_WORKING_HOUR * WAGE_PER_HOUR
-            print("Full time Employee is absent and his wage is: ", FULL_WORKING_HOUR * WAGE_PER_HOUR)
+            print("Full time Employee is present and his wage is: ", FULL_WORKING_HOUR * WAGE_PER_HOUR)
         elif random_number == 1:
+            total_hours += PART_TIME_WORKING_HOURS
+            total_days += 1
             total_wage += PART_TIME_WORKING_HOURS * WAGE_PER_HOUR
             print("Part time Employee is present and his wage is: ", PART_TIME_WORKING_HOURS * WAGE_PER_HOUR)
         elif random_number == 2:
@@ -20,9 +27,9 @@ def employee():
         else:
             print("Invalid")
 
-    return total_wage
+    return total_wage, total_days, total_hours
 
 
 if __name__ == "__main__":
     print("Welcome to employee payroll program")
-    print(employee())
+    print("Monthly wage and details of the employee is", employee())
